33eebeb47e1fae2458aaf0500e38985d2faf06fe
Aminul Islm <ai8728340@gmail.com>
2025-07-01 16:35:52 +0530

    redux itnregation complet

---
+++ src/app/store.ts
@@ -0,0 +1,28 @@
+// src/app/store.ts
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import authReducer from '../features/auth/AuthSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['auth'],
+};
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const persistor = persistStore(store);
---
+++ src/features/auth/AuthSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from '@reduxjs/toolkit';
+
+interface AuthState {
+    user: null | { email: string };
+    token: string | null;
+    isAuthenticated: boolean;
+}
+
+const initialState: AuthState = {
+    user: null,
+    token: null,
+    isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login(state, action: PayloadAction<{ user: { email: string }; token: string }>) {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.user = null;
+            state.token = null;
+            state.isAuthenticated = false;
+        },
+       
+    },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;

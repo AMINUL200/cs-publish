9f0503bf401602c7dd233b0a3f9db3b4a65ae239
Aminul Islam <ai8728340@example.com>
2026-07-13 14:59:50 +0530

    update:

--- src/pages/user/author_editor_hub/EditorHub.jsx
+++ src/pages/user/author_editor_hub/EditorHub.jsx
@@ -139,7 +139,7 @@ const EditorHub = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-yellow-50 to-red-50 py-12 sm:py-26">
+    <div className="min-h-screen bg-gradient-to-br from-yellow-50 to-red-50 py-12 sm:py-24">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header Section */}
         <div className="text-center mb-12">
--- src/pages/user/subscription/Subscription.jsx
+++ src/pages/user/subscription/Subscription.jsx
@@ -285,7 +285,7 @@ const Subscription = () => {
   console.log("pricingData:: ", pricingData);
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-0 sm:pt-30 pb-8 sm:pb-10 ">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-0 sm:pt-30 pb-8 sm:pb-10 pt-24">
       {/* Header */}
       <div className="text-center mb-12">
         <h2 className="text-3xl sm:text-4xl font-bold mb-4">
